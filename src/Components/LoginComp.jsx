3644f7cc619ca4ee09baecfe753575a5fdd27297
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-04-05 06:49:47 -0700

    a

--- src/Components/LoginComp.jsx
+++ src/Components/LoginComp.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const LoginComp = () => {
+const RegisterComp = () => {
   return (
     <div>
         <nav class="flex bg-zinc-950 fixed w-full">
-            <div class="flex gap-1 align-center text-center cursor-pointer ml-10 pt-6 pb-10">
+            <div class="flex gap-1 align-center text-center cursor-pointer ml-10 py-6">
                 <a href="/css/css/PROJECT2/Spotify.html"><p class="lg:text-3xl lg:font-bold text-white text-md">Pho<span className='text-green-600'>nart</span></p></a>
             </div>
         </nav>
@@ -14,19 +14,21 @@ const LoginComp = () => {
         <form className='flex justify-center items-center h-screen w-full'>
             <div className='w-full flex flex-col gap-7'>
                 <h2 className='text-center lg:text-4xl font-bold text-lg'>Login to Pho<span className='text-green-600'>nart</span></h2>
+                
 
                 <div className='flex flex-col lg:w-1/3 w-10/12  m-auto '>
-                    <p className='text-left lg:text-lg text-sm lg:font-bold font-medium pb-2'>Email: </p>
-                    <input type="text" placeholder="Full Name: " className="input input-bordered bg-zinc-950  rounded-lg lg:p-8 p-7" />
+                    <p className='text-left lg:text-lg text-xs lg:font-bold font-medium pb-2'>Email: </p>
+                    <input type="text" placeholder="Full Name: " className="input input-bordered bg-zinc-950 text-xs  rounded-lg lg:p-8 p-6" />
                 </div>
 
                 <div className='flex flex-col lg:w-1/3 w-10/12  m-auto '>
-                    <p className='text-left lg:text-lg text-sm lg:font-bold font-medium pb-2'>Password: </p>
-                    <input type="text" placeholder="Full Name: " className="input input-bordered bg-zinc-950  rounded-lg lg:p-8 p-7" />
+                    <p className='text-left lg:text-lg text-xs lg:font-bold font-medium pb-2'>Password: </p>
+                    <input type="text" placeholder="Full Name: " className="input input-bordered bg-zinc-950 text-xs  rounded-lg lg:p-8 p-6" />
                 </div>
-                <button className='bg-green-600 lg:w-1/3 w-10/12 m-auto p-4 rounded-lg'>Login</button>
+                <button className='bg-green-600 lg:w-1/3 w-10/12 m-auto p-3 rounded-lg text-sm'>Login</button>
 
-                <p className='lg:w-1/3 w-10/12  m-auto'>Dont have an account <Link to={'/register'}><span className='text-green-600'>Register</span></Link></p>
+                <p className='lg:w-1/3 w-10/12  m-auto text-xs'>Don't have an account? <Link to={'/'}><span className='text-green-600'>Register</span></Link></p>
+
             </div>
 
         </form>
@@ -34,4 +36,4 @@ const LoginComp = () => {
   )
 }
 
-export default LoginComp
+export default RegisterComp
